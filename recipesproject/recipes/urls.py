13aecad1e3dba152117dab189e002bcfc739313a
hasan-abir <contact.hasanabir@gmail.com>
2024-08-21 12:05:15 +0600

    Angular setup

--- recipesproject/recipes/urls.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import routers
-from .api import RecipeViewSet
-
-router = routers.DefaultRouter()
-router.register('api/recipes', RecipeViewSet)
-urlpatterns = router.urls
--- recipesproject/recipesproject/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('', include('frontend.urls')),
-    path('', include('recipes.urls')),
-    path('', include('accounts.urls')),
-    path('admin/', admin.site.urls)
-]
