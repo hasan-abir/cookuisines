d57bfbc9228eed3bb4f521669bed9083e3fc2338
hasan-abir <contact.hasanabir@gmail.com>
2024-08-23 11:44:58 +0600

    Formbuilder and validation

--- frontend/src/app/pages/recipemaker/recipemaker.component.ts
+++ frontend/src/app/pages/recipemaker/recipemaker.component.ts
@@ -1,11 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-recipemaker',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './recipemaker.component.html',
-  styleUrl: './recipemaker.component.css'
+  styleUrl: './recipemaker.component.css',
 })
 export class RecipemakerComponent {
+  makerForm = this.formBuilder.group({
+    title: ['', Validators.required],
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  onSubmit() {
+    if (this.makerForm.valid) {
+      console.log(this.makerForm.value);
+
+      this.makerForm.reset();
+    }
+  }
+
+  titleErrs(): { required: boolean } {
+    const required =
+      this.makerForm.controls.title.errors &&
+      this.makerForm.controls.title.errors['required'];
+
+    return {
+      required,
+    };
+  }
 }
