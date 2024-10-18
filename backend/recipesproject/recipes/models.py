187727626f3f0285b569f536ee5326e6b79dbeb9
hasan-abir <contact.hasanabir@gmail.com>
2024-10-18 11:58:37 +0600

    Models and tests

--- backend/recipesproject/recipes/models.py
+++ backend/recipesproject/recipes/models.py
@@ -1,3 +1,65 @@
 from django.db import models
 
 # Create your models here.
+
+class Recipe(models.Model):
+    EASY = "easy"
+    MEDIUM = "medium"
+    HARD = "hard"
+    difficulty_choices = {
+        EASY: "Easy",
+        MEDIUM: "Medium",
+        HARD: "Hard",
+    }
+    title = models.CharField(max_length=100, blank=False, null=False)
+    preparation_time = models.DurationField(blank=False, null=True)
+    cooking_time = models.DurationField(blank=False, null=True)
+    difficulty = models.CharField(max_length=6, choices=difficulty_choices, blank=False, null=True)
+    image_id = models.CharField(max_length=500, blank=False, null=False)
+    image_url = models.URLField(max_length=2000,blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_at']
+
+class RecipeIngredient(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+    quantity = models.CharField(max_length=100, blank=False, null=False)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='ingredients')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_at']
+
+class RecipeInstruction(models.Model):
+    step = models.TextField(blank=False, null=False)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='instructions')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_at']
+
+class RecipeMealType(models.Model):
+    breakfast = models.BooleanField(default=False)
+    brunch = models.BooleanField(default=False)
+    lunch = models.BooleanField(default=False)
+    dinner = models.BooleanField(default=False)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='meal_types')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_at']
+
+class RecipeDietaryPreference(models.Model):
+    vegan = models.BooleanField(default=False)
+    glutenfree = models.BooleanField(default=False)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='dietary_preferences')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_at']
--- backend/recipesproject/recipes/test_suites/test_models.py
+++ backend/recipesproject/recipes/test_suites/test_models.py
@@ -1,2 +1,93 @@
 from django.test import TestCase
+from recipes.models import Recipe, RecipeIngredient, RecipeInstruction, RecipeMealType, RecipeDietaryPreference
+from datetime import timedelta
 
+# Create your tests here.
+class RecipeIngredientTestCase(TestCase):
+    def test_default_values(self):
+        data = {
+            'name': 'Ingredient 1',
+            'quantity': '2 spoon',
+            'recipe': get_demo_recipe()
+        }
+        recipe_ingredient = RecipeIngredient.objects.create(**data)
+
+        saved_recipe_ingredients = RecipeIngredient.objects.get(pk=recipe_ingredient.pk)
+        self.assertEqual(saved_recipe_ingredients.name, recipe_ingredient.name)
+        self.assertEqual(saved_recipe_ingredients.quantity, recipe_ingredient.quantity)
+        self.assertEqual(saved_recipe_ingredients.recipe.pk, recipe_ingredient.recipe.pk)
+        self.assertTrue(saved_recipe_ingredients.created_at)
+        self.assertTrue(saved_recipe_ingredients.updated_at)
+
+class RecipeInstructionTestCase(TestCase):
+    def test_default_values(self):
+        data = {
+            'step': 'Instruction 1',
+            'recipe': get_demo_recipe()
+        }
+        recipe_instruction = RecipeInstruction.objects.create(**data)
+
+        saved_recipe_instructions = RecipeInstruction.objects.get(pk=recipe_instruction.pk)
+        self.assertEqual(saved_recipe_instructions.step, recipe_instruction.step)
+        self.assertEqual(saved_recipe_instructions.recipe.pk, recipe_instruction.recipe.pk)
+        self.assertTrue(saved_recipe_instructions.created_at)
+        self.assertTrue(saved_recipe_instructions.updated_at)
+
+class RecipeMealTypeTestCase(TestCase):
+    def test_default_values(self):
+        data = {
+            'breakfast': True,
+            'brunch': False,
+            'lunch': True,
+            'dinner': False,
+            'recipe': get_demo_recipe()
+        }
+        recipe_mealtype = RecipeMealType.objects.create(**data)
+
+        saved_recipe_mealtype = RecipeMealType.objects.get(pk=recipe_mealtype.pk)
+        self.assertEqual(saved_recipe_mealtype.breakfast, recipe_mealtype.breakfast)
+        self.assertEqual(saved_recipe_mealtype.brunch, recipe_mealtype.brunch)
+        self.assertEqual(saved_recipe_mealtype.lunch, recipe_mealtype.lunch)
+        self.assertEqual(saved_recipe_mealtype.dinner, recipe_mealtype.dinner)
+        self.assertEqual(saved_recipe_mealtype.recipe.pk, recipe_mealtype.recipe.pk)
+        self.assertTrue(saved_recipe_mealtype.created_at)
+        self.assertTrue(saved_recipe_mealtype.updated_at)
+
+class RecipeDietaryPreferenceTestCase(TestCase):
+    def test_default_values(self):
+        data = {
+            'vegan': True,
+            'glutenfree': False,
+            'recipe': get_demo_recipe()
+        }
+        recipe_dietarypreference = RecipeDietaryPreference.objects.create(**data)
+
+        saved_recipe_dietarypreference = RecipeDietaryPreference.objects.get(pk=recipe_dietarypreference.pk)
+        self.assertEqual(saved_recipe_dietarypreference.vegan, recipe_dietarypreference.vegan)
+        self.assertEqual(saved_recipe_dietarypreference.glutenfree, recipe_dietarypreference.glutenfree)
+        self.assertEqual(saved_recipe_dietarypreference.recipe.pk, recipe_dietarypreference.recipe.pk)
+        self.assertTrue(saved_recipe_dietarypreference.created_at)
+        self.assertTrue(saved_recipe_dietarypreference.updated_at)
+
+class RecipeTestCase(TestCase):
+    def test_default_values(self):
+        recipe = get_demo_recipe()
+
+        saved_recipe = Recipe.objects.get(pk=recipe.pk)
+        self.assertEqual(saved_recipe.title, recipe.title)
+        self.assertEqual(saved_recipe.preparation_time, recipe.preparation_time)
+        self.assertEqual(saved_recipe.cooking_time, recipe.cooking_time)
+        self.assertEqual(saved_recipe.difficulty, recipe.difficulty)
+        self.assertEqual(saved_recipe.image_id, recipe.image_id)
+        self.assertEqual(saved_recipe.image_url, recipe.image_url)
+
+def get_demo_recipe():
+    data = {
+            'title': 'Example Recipe',
+            'preparation_time': timedelta(hours=0, minutes=1, seconds=30),
+            'cooking_time': timedelta(hours=0, minutes=1, seconds=30),
+            'difficulty': 'Easy',
+            'image_id': '123',
+            'image_url': 'http://test.com/images/123',
+        }
+    return Recipe.objects.create(**data)
