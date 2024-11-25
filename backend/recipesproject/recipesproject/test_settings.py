ae658841be0d5fd83682c49cf61e105d222fc0f4
hasan-abir <contact.hasanabir@gmail.com>
2024-11-25 10:31:22 +0600

    Postgres SETUP

--- backend/recipesproject/recipesproject/test_settings.py
+++ backend/recipesproject/recipesproject/test_settings.py
@@ -103,13 +103,9 @@ WSGI_APPLICATION = 'recipesproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv('DB_NAME'),
-        "USER": os.getenv('DB_USER'),
-        "PASSWORD": os.getenv('DB_PASSWORD'),
-        "HOST": os.getenv('DB_HOST'),
-        "PORT": os.getenv('DB_PORT'),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
