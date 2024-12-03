b668d64b6b4937872a25bf0ae005f49b388b6b49
hasan-abir <contact.hasanabir@gmail.com>
2024-12-03 12:09:34 +0600

    AuthService

---
+++ frontend/src/app/services/auth.service.spec.ts
@@ -0,0 +1,71 @@
+import { TestBed } from '@angular/core/testing';
+
+import { provideHttpClient } from '@angular/common/http';
+import {
+  provideHttpClientTesting,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { AuthService } from './auth.service';
+
+describe('AuthService', () => {
+  let service: AuthService;
+  let httpTesting: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    });
+    service = TestBed.inject(AuthService);
+    httpTesting = TestBed.inject(HttpTestingController);
+  });
+  afterEach(() => {
+    TestBed.inject(HttpTestingController).verify();
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('login: should call API', () => {
+    const body = {
+      username: 'test_user',
+      password: 'testtest',
+    };
+    service.login(body).subscribe();
+
+    const req = httpTesting.expectOne(
+      'https://cookuisines.onrender.com/api-token-obtain'
+    );
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toBe(body);
+  });
+
+  it('signup: should call API', () => {
+    const body = {
+      username: 'test_user',
+      email: 'test@test.com',
+      password: 'testtest',
+    };
+    service.signup(body).subscribe();
+
+    const req = httpTesting.expectOne(
+      'https://cookuisines.onrender.com/api-token-register'
+    );
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toBe(body);
+  });
+
+  it('refresh: should call API', () => {
+    const body = {
+      username: 'test_user',
+      password: 'testtest',
+    };
+    service.refresh(body).subscribe();
+
+    const req = httpTesting.expectOne(
+      'https://cookuisines.onrender.com/api-token-refresh'
+    );
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toBe(body);
+  });
+});
---
+++ frontend/src/app/services/auth.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
+
+export interface LoginBody {
+  username: string;
+  password: string;
+}
+
+export interface SignupBody extends LoginBody {
+  email: string;
+}
+
+export interface UserResponse {
+  username: string;
+  email: string;
+}
+
+export interface TokenResponse {
+  access: string;
+  refresh: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  url = 'https://cookuisines.onrender.com/';
+
+  constructor(private http: HttpClient) {}
+
+  login(body: LoginBody): Observable<UserResponse> {
+    return this.http.post<UserResponse>(this.url + 'api-token-obtain', body);
+  }
+
+  signup(body: SignupBody): Observable<UserResponse> {
+    return this.http.post<UserResponse>(this.url + 'api-token-register', body);
+  }
+
+  refresh(body: LoginBody): Observable<TokenResponse> {
+    return this.http.post<TokenResponse>(this.url + 'api-token-refresh', body);
+  }
+}
