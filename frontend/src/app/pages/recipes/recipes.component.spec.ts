053b2688ab2ddc47dac58c29e340c9cacce3e272
hasan-abir <contact.hasanabir@gmail.com>
2025-01-27 11:50:15 +0600

    All recipe filters with tests

--- frontend/src/app/pages/recipes/recipes.component.spec.ts
+++ frontend/src/app/pages/recipes/recipes.component.spec.ts
@@ -8,25 +8,31 @@ import {
 import { Observable, timer } from 'rxjs';
 import { RecipeService } from '../../services/recipe.service';
 import { RecipesComponent } from './recipes.component';
+import { provideRouter, Router } from '@angular/router';
+import { routes } from '../../app.routes';
 
 describe('RecipeComponent', () => {
   let component: RecipesComponent;
   let fixture: ComponentFixture<RecipesComponent>;
   let compiled: HTMLElement;
   let recipeServiceSpy: jasmine.SpyObj<RecipeService>;
+  let router: Router;
 
   beforeEach(async () => {
     const getMethodSpy = jasmine.createSpyObj('RecipeService', ['get_recipes']);
 
     await TestBed.configureTestingModule({
       imports: [RecipesComponent],
-      providers: [{ provide: RecipeService, useValue: getMethodSpy }],
+      providers: [
+        provideRouter(routes),
+        { provide: RecipeService, useValue: getMethodSpy },
+      ],
     }).compileComponents();
 
     recipeServiceSpy = TestBed.inject(
       RecipeService
     ) as jasmine.SpyObj<RecipeService>;
-
+    router = TestBed.inject(Router);
     recipeServiceSpy.get_recipes.and.returnValue(
       new Observable((subscriber) => {
         subscriber.next({
@@ -174,6 +180,210 @@ describe('RecipeComponent', () => {
     expect(recipeServiceSpy.get_recipes).toHaveBeenCalledWith(nextPage);
     expect(recipes.length).toBe(2);
     expect(noMoreRecipes).toBeTruthy();
+    expect(loader).toBeFalsy();
+  }));
+
+  it('should search by title, ingredient, breakfast, brunch, lunch, dinner, vegan, glutenfree', fakeAsync(() => {
+    spyOn(router, 'navigate');
+
+    const title = 'example';
+    const ingredient = 'exampleingredient';
+    const titleInput = compiled.querySelector(
+      '[formControlName="title"'
+    ) as HTMLInputElement;
+    const ingredientInput = compiled.querySelector(
+      '[formControlName="ingredient"'
+    ) as HTMLInputElement;
+    const breakfastInput = compiled.querySelector(
+      '[formControlName="breakfast"'
+    ) as HTMLInputElement;
+    const brunchInput = compiled.querySelector(
+      '[formControlName="brunch"'
+    ) as HTMLInputElement;
+    const lunchInput = compiled.querySelector(
+      '[formControlName="lunch"'
+    ) as HTMLInputElement;
+    const dinnerInput = compiled.querySelector(
+      '[formControlName="dinner"'
+    ) as HTMLInputElement;
+    const veganInput = compiled.querySelector(
+      '[formControlName="vegan"'
+    ) as HTMLInputElement;
+    const glutenfreeInput = compiled.querySelector(
+      '[formControlName="glutenfree"'
+    ) as HTMLInputElement;
+    const formSubmitBtn = compiled.querySelector(
+      '[type="submit"'
+    ) as HTMLButtonElement;
+
+    recipeServiceSpy.get_recipes.and.returnValue(
+      new Observable((subscriber) => {
+        timer(2000).subscribe(() => {
+          subscriber.next({
+            count: 2,
+            next: null,
+            previous: null,
+            results: [
+              {
+                url: 'http://testserver/recipes/1/',
+                title: 'Recipe 1',
+                preparation_time: '00:01:40',
+                cooking_time: '01:30:00',
+                difficulty: 'easy',
+                ingredients: 'http://testserver/recipes/123/ingredients/',
+                instructions: 'http://testserver/recipes/123/instructions/',
+                meal_type: 'http://testserver/recipes/mealtypes/123/',
+                dietary_preference:
+                  'http://testserver/recipes/dietarypreferences/123/',
+                created_by_username: 'hasan_abir',
+                image_id: '456',
+                image_url: 'http://testserver/images/123',
+              },
+
+              {
+                url: 'http://testserver/recipes/2/',
+                title: 'Recipe 2',
+                preparation_time: '00:01:40',
+                cooking_time: '01:30:00',
+                difficulty: 'easy',
+                ingredients: 'http://testserver/recipes/123/ingredients/',
+                instructions: 'http://testserver/recipes/123/instructions/',
+                meal_type: 'http://testserver/recipes/mealtypes/123/',
+                dietary_preference:
+                  'http://testserver/recipes/dietarypreferences/123/',
+                created_by_username: 'hasan_abir',
+                image_id: '456',
+                image_url: 'http://testserver/images/123',
+              },
+            ],
+          });
+        });
+      })
+    );
+
+    titleInput.value = title;
+    titleInput.dispatchEvent(new Event('input'));
+    ingredientInput.value = ingredient;
+    ingredientInput.dispatchEvent(new Event('input'));
+    breakfastInput.click();
+    brunchInput.click();
+    lunchInput.click();
+    dinnerInput.click();
+    veganInput.click();
+    glutenfreeInput.click();
+
+    formSubmitBtn.click();
+    fixture.detectChanges();
+
+    let loader = compiled.querySelector('.loader');
+    expect(loader).toBeTruthy();
+
+    tick(2000);
+    fixture.detectChanges();
+    const recipes = compiled.querySelectorAll('.cell');
+    loader = compiled.querySelector('.loader');
+    expect(router.navigate).toHaveBeenCalledWith(['recipes'], {
+      queryParams: {
+        title,
+        ingredient,
+        breakfast: true,
+        brunch: true,
+        lunch: true,
+        dinner: true,
+        vegan: true,
+        glutenfree: true,
+      },
+    });
+    expect(recipeServiceSpy.get_recipes).toHaveBeenCalledWith(
+      `recipes/?title=${title}&ingredient=${ingredient}&breakfast=true&brunch=true&lunch=true&dinner=true&vegan=true&glutenfree=true`
+    );
+    expect(recipes.length).toBe(2);
+    expect(loader).toBeFalsy();
+  }));
+
+  it('should search by only some of the fields', fakeAsync(() => {
+    spyOn(router, 'navigate');
+
+    const title = 'example';
+    const titleInput = compiled.querySelector(
+      '[formControlName="title"'
+    ) as HTMLInputElement;
+    const dinnerInput = compiled.querySelector(
+      '[formControlName="dinner"'
+    ) as HTMLInputElement;
+    const formSubmitBtn = compiled.querySelector(
+      '[type="submit"'
+    ) as HTMLButtonElement;
+
+    recipeServiceSpy.get_recipes.and.returnValue(
+      new Observable((subscriber) => {
+        timer(2000).subscribe(() => {
+          subscriber.next({
+            count: 2,
+            next: null,
+            previous: null,
+            results: [
+              {
+                url: 'http://testserver/recipes/1/',
+                title: 'Recipe 1',
+                preparation_time: '00:01:40',
+                cooking_time: '01:30:00',
+                difficulty: 'easy',
+                ingredients: 'http://testserver/recipes/123/ingredients/',
+                instructions: 'http://testserver/recipes/123/instructions/',
+                meal_type: 'http://testserver/recipes/mealtypes/123/',
+                dietary_preference:
+                  'http://testserver/recipes/dietarypreferences/123/',
+                created_by_username: 'hasan_abir',
+                image_id: '456',
+                image_url: 'http://testserver/images/123',
+              },
+
+              {
+                url: 'http://testserver/recipes/2/',
+                title: 'Recipe 2',
+                preparation_time: '00:01:40',
+                cooking_time: '01:30:00',
+                difficulty: 'easy',
+                ingredients: 'http://testserver/recipes/123/ingredients/',
+                instructions: 'http://testserver/recipes/123/instructions/',
+                meal_type: 'http://testserver/recipes/mealtypes/123/',
+                dietary_preference:
+                  'http://testserver/recipes/dietarypreferences/123/',
+                created_by_username: 'hasan_abir',
+                image_id: '456',
+                image_url: 'http://testserver/images/123',
+              },
+            ],
+          });
+        });
+      })
+    );
+
+    titleInput.value = title;
+    titleInput.dispatchEvent(new Event('input'));
+    dinnerInput.click();
+
+    formSubmitBtn.click();
+    fixture.detectChanges();
+
+    let loader = compiled.querySelector('.loader');
+    expect(loader).toBeTruthy();
+
+    tick(2000);
+    fixture.detectChanges();
+    const recipes = compiled.querySelectorAll('.cell');
+    loader = compiled.querySelector('.loader');
+    expect(router.navigate).toHaveBeenCalledWith(['recipes'], {
+      queryParams: {
+        title,
+        dinner: true,
+      },
+    });
+    expect(recipeServiceSpy.get_recipes).toHaveBeenCalledWith(
+      `recipes/?title=${title}&dinner=true`
+    );
+    expect(recipes.length).toBe(2);
     expect(loader).toBeFalsy();
   }));
 
--- frontend/src/app/pages/recipes/recipes.component.ts
+++ frontend/src/app/pages/recipes/recipes.component.ts
@@ -8,7 +8,18 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+
+interface QueryParams {
+  title: string;
+  ingredient: string;
+  breakfast: boolean;
+  brunch: boolean;
+  lunch: boolean;
+  dinner: boolean;
+  vegan: boolean;
+  glutenfree: boolean;
+}
 
 @Component({
   selector: 'app-recipes',
@@ -27,27 +38,65 @@ export class RecipesComponent {
   };
 
   searchForm = this.formBuilder.group({
-    title: new FormControl<string | null>(null),
+    title: new FormControl<string>(''),
+    ingredient: new FormControl<string>(''),
+    breakfast: new FormControl<boolean>(false),
+    brunch: new FormControl<boolean>(false),
+    lunch: new FormControl<boolean>(false),
+    dinner: new FormControl<boolean>(false),
+    vegan: new FormControl<boolean>(false),
+    glutenfree: new FormControl<boolean>(false),
   });
 
   constructor(
     private recipeService: RecipeService,
     private formBuilder: FormBuilder,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit() {
     this.fetchRecipes(this.router.url.replace('/', ''));
+    this.route.queryParams.subscribe((params) => {
+      Object.keys(params).forEach((key) => {
+        this.searchForm.get(key)?.setValue(params[key]);
+      });
+    });
   }
 
   onSearch() {
-    if (this.searchForm.valid) {
-      this.router.navigate(['recipes'], {
-        queryParams: { title: this.searchForm.value.title },
-      });
+    const value = this.searchForm.value as QueryParams;
 
-      this.fetchRecipes('recipes/?title=' + this.searchForm.value.title, true);
-    }
+    const queryParams: QueryParams = {
+      title: value.title,
+      ingredient: value.ingredient,
+      breakfast: value.breakfast,
+      brunch: value.brunch,
+      lunch: value.lunch,
+      dinner: value.dinner,
+      vegan: value.vegan,
+      glutenfree: value.glutenfree,
+    };
+
+    const filteredQueryParams = Object.fromEntries(
+      Object.entries(queryParams).filter(([key, value]) => value)
+    );
+
+    this.router.navigate(['recipes'], { queryParams: filteredQueryParams });
+
+    let url = 'recipes/?';
+
+    Object.keys(filteredQueryParams).forEach((key, index) => {
+      const param = key as keyof QueryParams;
+
+      if (index > 0 && !url.endsWith('?')) {
+        url += '&';
+      }
+
+      url += `${param}=${queryParams[param]}`;
+    });
+
+    this.fetchRecipes(url, true);
   }
 
   fetchMoreRecipes() {
