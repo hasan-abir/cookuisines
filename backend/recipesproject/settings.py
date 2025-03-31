9739d6390353c53313f5c152021d05eddb51b904
hasan-abir <contact.hasanabir@gmail.com>
2025-03-31 11:36:36 +0600

    Pre-deploy settings

--- backend/recipesproject/settings.py
+++ backend/recipesproject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', os.getenv('BACKEND_HOST')]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', os.getenv('BACKEND_HOST'), os.getenv('FRONTEND_HOST')]
 
 
 # Application definition
